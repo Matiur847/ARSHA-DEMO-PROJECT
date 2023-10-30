6d735419134e23a942dd383f5f2297103801c4d0
Matiur847 <mclaren847329@gmail.com>
2023-10-30 21:23:22 +0600

    adding some property

--- src/componnents/Contact/Contact.js
+++ src/componnents/Contact/Contact.js
@@ -34,7 +34,7 @@ const Contact = () => {
                                 <span>A108 Adam Street, New York, NY 535022</span>
                             </div>
                             <div className="maps-section mt-4">
-                                <iframe id="inlineFrameExample" title="Inline Frame Example" width='370' height='300' src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"> </iframe>
+                                <iframe id="inlineFrameExample" title="Inline Frame Example"  height='300' src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"> </iframe>
                             </div>
                         </div>
                     </Col>
